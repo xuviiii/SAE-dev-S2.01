ad3fee8fda52fef28681cefe890c6f7141ddc212
gmartin70 <gabrielmartin7002@gmail.com>
2026-05-13 16:06:35 +0200

    drag vonctionnelle

--- SAE_Tower_defense/src/main/java/universite_paris8/iut/vxu/sae_tower_defense/Controller.java
+++ SAE_Tower_defense/src/main/java/universite_paris8/iut/vxu/sae_tower_defense/Controller.java
@@ -1,8 +1,11 @@
 package universite_paris8.iut.vxu.sae_tower_defense;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -10,6 +13,7 @@ import javafx.scene.shape.*;
 import javafx.scene.shape.Shape;
 
 import java.net.URL;
+import java.security.cert.CertificateNotYetValidException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -17,6 +21,9 @@ public class Controller implements Initializable {
 
     @FXML
     private TilePane terrain;
+
+    @FXML
+    private Pane tour;
 
 
     public void créerTerrain() {
@@ -33,9 +40,45 @@ public class Controller implements Initializable {
         }
     }
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         map = new Map();
         créerTerrain();
+
+        tour.setOnDragDetected(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Dragboard db = tour.startDragAndDrop(TransferMode.ANY);
+                ClipboardContent content = new ClipboardContent();
+                content.putString("flêche");
+                db.setContent(content);
+                event.consume();
+            }
+        });
+
+        terrain.setOnDragOver(new EventHandler<DragEvent>() {
+            @Override public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                if (db.hasString()) {
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+            }
+        });
+
+        terrain.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                Rectangle t;
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    System.out.println(db.getString());
+                }
+                event.setDropCompleted(success);
+                event.consume();
+            }
+        });
     }
 }
